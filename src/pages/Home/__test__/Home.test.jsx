8705e308f5c5f28e353e3078abb16c2e48ad8e10
Lipezin <lipezin@mail.ru>
2021-12-06 18:20:47 +0700

    finale

--- src/pages/Home/__test__/Home.test.jsx
+++ src/pages/Home/__test__/Home.test.jsx
@@ -1,16 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import { Home } from "../Home"
-import { BrowserRouter } from 'react-router-dom';
-import { Routes, Route, MemoryRouter } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
 
 describe("test Home component", () => {
     it("render text", () => {
-        const componentHome = render(
+        render(
             <MemoryRouter initialEntries={["/"]}>
                 <Home />
             </MemoryRouter>
         )
-        const textTitle = componentHome.getByText("Домашняя страница");
+
+        const textTitle = screen.getByText("Домашняя страница");
         expect(textTitle).toBeInTheDocument()
     })
 })
